c17f6f7f8ab7ba8263a6c126e0dad45e364426c9
Kenan Agaev <kenkosh@yandex.ru>
2020-03-19 22:38:46 +0300

    Завершает верстку мобильной версии каталога

--- source/js/menu.js
+++ source/js/menu.js
@@ -4,14 +4,23 @@
   const pageHeader = document.querySelector('.page-header');
   const nav = pageHeader.querySelector('.main-nav');
   const navMenu = nav.querySelector('.main-nav__menu');
+  const navLinks = nav.querySelectorAll('.main-nav__link');
   const buttonToggler = nav.querySelector('.toggler');
 
-  const toggleMenu = function () {
+  const closeMenu = () => {
     navMenu.classList.toggle('main-nav__menu--active');
     buttonToggler.classList.toggle('toggler--active');
     pageHeader.classList.toggle('page-header--expanded');
   };
 
-  buttonToggler.addEventListener('click', toggleMenu);
+  const onToggleButtonCloseMenu = () => {
+    closeMenu();
+  };
+
+  navLinks.forEach((it) => {
+    it.addEventListener('click', closeMenu)
+  });
+
+  buttonToggler.addEventListener('click', onToggleButtonCloseMenu);
 
 })();

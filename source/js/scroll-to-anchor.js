f468ca22b4782974c8ddd7fceb5f993dd2e17eac
Kenan Agaev <kenkosh@yandex.ru>
2020-03-31 20:23:48 +0300

    Фиксит плавную прокрутку к якорю на мобильных

--- source/js/scroll-to-anchor.js
+++ source/js/scroll-to-anchor.js
@@ -5,20 +5,42 @@
   const mainNavList = document.querySelector('.main-nav__list');
   const anchors = mainNavList.querySelectorAll('a[href*="#"]');
 
+  let timeout;
+
+  const scrollToAnchor = (id) => {
+    document.getElementById(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  };
+
   const onAnchorClickDocumentScroll = (evt) => {
     evt.preventDefault();
 
     let blockId = evt.target.getAttribute('href').substr(1);
-
-    document.getElementById(blockId).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-
-  };
-
-  for (let anchor of anchors) {
-    anchor.addEventListener('click', onAnchorClickDocumentScroll);
+    console.log('blockId:', blockId)
+    scrollToAnchor(blockId);
   }
+
+  const onWindowResiseSetScroll = () => {
+    if (window.matchMedia("(min-width: 678px)").matches) {
+      console.log('good')
+
+      for (let anchor of anchors) {
+        anchor.addEventListener('click', function (evt) {
+          onAnchorClickDocumentScroll(evt);
+        });
+      }
+
+    }
+  }
+
+  window.addEventListener('resize', function () {
+    if (timeout) {
+      this.clearTimeout(timeout);
+    }
+
+    timeout = this.setTimeout(onWindowResiseSetScroll, 500);
+  })
 
 })();

de1e1d3396d0ca90a64323bd884dbfb4e40a5578
Kenan Agaev <kenkosh@yandex.ru>
2020-04-03 19:56:46 +0300

    minor changes

--- source/js/scroll-to-anchor.js
+++ source/js/scroll-to-anchor.js
@@ -5,8 +5,6 @@
   const mainNavList = document.querySelector('.main-nav__list');
   const anchors = mainNavList.querySelectorAll('a[href*="#"]');
 
-  let timeout;
-
   const scrollToAnchor = (id) => {
     document.getElementById(id).scrollIntoView({
       behavior: 'smooth',
@@ -18,29 +16,13 @@
     evt.preventDefault();
 
     let blockId = evt.target.getAttribute('href').substr(1);
-    console.log('blockId:', blockId)
     scrollToAnchor(blockId);
   }
 
-  const onWindowResiseSetScroll = () => {
-    if (window.matchMedia("(min-width: 678px)").matches) {
-      console.log('good')
-
-      for (let anchor of anchors) {
-        anchor.addEventListener('click', function (evt) {
-          onAnchorClickDocumentScroll(evt);
-        });
-      }
-
-    }
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (evt) {
+      onAnchorClickDocumentScroll(evt);
+    });
   }
-
-  window.addEventListener('resize', function () {
-    if (timeout) {
-      this.clearTimeout(timeout);
-    }
-
-    timeout = this.setTimeout(onWindowResiseSetScroll, 500);
-  })
 
 })();
